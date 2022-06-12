96c776a83139f5546e104f99f205bf73ed06bb45
SyluxDX <syluxdx@gmail.com>
2022-06-12 18:41:22 +0100

    Create write_file.py

---
+++ write_file.py
@@ -0,0 +1,9 @@
+""" write file """
+import random
+
+letters = "".join(random.choice(string.ascii_lowercase) for i in range(10))
+
+with open(f"filename_{letters}.txt", "w", encoding="utf8") as fp:
+  fp.write(''.join(random.choice(string.ascii_lowercase) for i in range(100)))
+
+print(f"Written filename_{letters}.txt") 
